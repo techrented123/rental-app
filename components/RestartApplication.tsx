7247a6945a106fc0d70dc6a6cb9c689bdf8a42ba
Tambi <tambi.asawo@verticalmotion.ca>
2025-06-02 14:05:03 -0700

    feat:clear localstorage on application restart

---
+++ components/RestartApplication.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { RotateCcw } from "lucide-react";
+import { useRentalApplicationContext } from "@/contexts/rental-application-context";
+import AlertDialogBox from "./ui/alert-dialog";
+
+const RestartApplication = () => {
+  const { restartApplication } = useRentalApplicationContext();
+
+  const handleRestart = () => {
+    restartApplication();
+  };
+
+  return (
+    <div>
+      <AlertDialogBox
+        title={"Are you sure?"}
+        description="You will lose all your progress including all the documents you
+              uploaded"
+        proceedBtnText="Yes, restart application"
+        onProceed={handleRestart}
+      >
+        <button className="flex gap-2 px-4 py-2 border border-[ring-offset-color] hover:bg-accent hover:text-accent-foreground ring-offset-background rounded transition-colors">
+          <RotateCcw /> Restart Application
+        </button>
+      </AlertDialogBox>
+    </div>
+  );
+};
+
+export default RestartApplication;
