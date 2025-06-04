6fd51838aafd054a1ab702b28202b1dc26a9cbd9
Tambi <tambi.asawo@verticalmotion.ca>
2025-06-04 13:24:03 -0700

    fix: add suspense boundary

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import PropertyListing from "@/components/property-listing";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <div className="!overflow-y-auto h-screen">
-      <PropertyListing />
-    </div>
+    <Suspense fallback={<p>Loading query params…</p>}>
+      <div className="!overflow-y-auto h-screen">
+        <PropertyListing />
+      </div>
+    </Suspense>
   );
 }
