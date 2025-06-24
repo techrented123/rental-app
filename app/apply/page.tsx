6616b3c9d3ec2a9867bf3f2c4b0976cbec6e987c
Tambi <tambi.asawo@verticalmotion.ca>
2025-06-24 16:57:06 -0700

    feat: confirm. dialog bix

--- app/apply/page.tsx
+++ app/apply/page.tsx
@@ -2,7 +2,6 @@
 import * as React from "react";
 import { ChevronLeft } from "lucide-react";
 import FormStepper from "@/components/ui/stepper";
-import Link from "next/link";
 import {
   UserCircle,
   Building2,
@@ -18,6 +17,9 @@ import BackgroundCheck from "@/components/BackgroundCheck";
 import ApplicationForm from "@/components/ApplicationForm";
 import { RentalAgreement } from "@/components/Documents/RentalAgreement";
 import SubmitApplication from "@/components/SubmitApplication";
+import AlertDialogBox from "@/components/ui/alert-dialog";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
 
 const steps = [
   {
@@ -76,7 +78,8 @@ const steps = [
 ];
 
 export default function Home() {
-  const [slug, setSlug] = React.useState(""); //
+  const router = useRouter();
+  const [slug, setSlug] = React.useState("");
   const [lastSavedStep, setLastSavedStep] = React.useState(0);
 
   React.useEffect(() => {
@@ -91,16 +94,22 @@ export default function Home() {
     }
   }, []);
   const handleClick = () => {
-    console.log(343)
+    router.push(`/?slug=${slug}`);
   };
 
   return (
     <div className="md:mt-3 flex-col md:flex justify-start h-screen items-start gap-4 md:gap-0">
-      <div className="p-2 md:hover:bg-blue-400 md:hover:text-white ml-2.5 w-[40px] h-[40px] rounded-full ">
-        <Link href={`/?slug=${slug}`} className="" onClick={handleClick}>
+      <AlertDialogBox
+        title={"Your progress will be saved"}
+        description="No need to worry. Your progress will be saved and you can continue from where you left off"
+        proceedBtnText="Continue"
+        onProceed={handleClick}
+      >
+        <Button className="md:bg-blue-500 p-2 md:hover:bg-blue-400 md:hover:text-white ml-2.5 w-[40px] h-[40px] rounded-full">
           <ChevronLeft />
-        </Link>
-      </div>
+        </Button>
+      </AlertDialogBox>
+
       <FormStepper steps={steps} lastSavedStep={lastSavedStep} />
     </div>
   );
